5c65cde2f1a96f136dea7737a0dbe7fd3533a515
Daniel Hopson <hdhopson88@yahoo.com>
2022-03-03 23:31:16 -0600

    Updates home component, removes unnecessary components/services

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,5 @@
 import { Component } from '@angular/core';
 
-import { HeaderComponent } from './components/header/header.component'
-import { SiteIconsComponent } from './components/site-icons/site-icons.component';
-import { SocialIconsComponent } from './components/social-icons/social-icons.component';
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',

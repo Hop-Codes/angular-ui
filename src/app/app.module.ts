7eab699a2ee98db95e4f0d1909b5e7465ba30d4e
Daniel Hopson <hdhopson88@yahoo.com>
2022-03-19 01:22:50 -0500

    Adds navbar and footer components to module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,13 +7,17 @@ import { AppComponent } from './app.component';
 import { HomePageComponent } from './components/home-page/home-page.component';
 import { ContactPageComponent } from './components/contact-page/contact-page.component';
 import { AboutPageComponent } from './components/about-page/about-page.component';
+import { NavbarItemsComponent } from './components/navbar-items/navbar-items.component';
+import { FooterItemsComponent } from './components/footer-items/footer-items.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomePageComponent,
     ContactPageComponent,
-    AboutPageComponent
+    AboutPageComponent,
+    NavbarItemsComponent,
+    FooterItemsComponent
   ],
   imports: [
     BrowserModule,

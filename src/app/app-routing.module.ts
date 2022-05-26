a017b8c610beb7e5765d45c7f2f2b07641834404
Daniel Hopson <hdhopson88@yahoo.com>
2022-05-26 15:48:21 -0500

    Creates and adds navigation to news component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,14 +4,17 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomePageComponent } from './components/home-page/home-page.component';
 import { ContactPageComponent } from './components/contact-page/contact-page.component';
 import { AboutPageComponent } from './components/about-page/about-page.component'
+import { SubsidiariesComponent } from './components/subsidiaries/subsidiaries.component';
+import { CommunityServiceComponent } from './components/community-service/community-service.component';
+import { NewsComponent } from './components/news/news.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { component: HomePageComponent, path: 'home' },
-  { component: AboutPageComponent, path: 'subsidiaries' },
+  { component: SubsidiariesComponent, path: 'subsidiaries' },
   { component: AboutPageComponent, path: 'about' },
-  { component: AboutPageComponent, path: 'community-service' },
-  { component: AboutPageComponent, path: 'news' }
+  { component: CommunityServiceComponent, path: 'community-service' },
+  { component: NewsComponent, path: 'news' }
 ];
 
 @NgModule({

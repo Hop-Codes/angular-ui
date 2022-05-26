927fff43a89eb992465bee4efe00d5172b6aa485
Daniel Hopson <hdhopson88@yahoo.com>
2022-05-26 15:55:35 -0500

    Creates community service component

---
+++ src/app/components/community-service/community-service.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { CommunityServiceComponent } from './community-service.component';
+
+describe('CommunityServiceComponent', () => {
+  let component: CommunityServiceComponent;
+  let fixture: ComponentFixture<CommunityServiceComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ CommunityServiceComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(CommunityServiceComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
